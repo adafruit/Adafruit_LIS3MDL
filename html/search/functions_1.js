27dceaf4ff2de79cadf703daaf1d7d931812bb7b
Travis CI <travis@travis-ci.org>
2019-12-22 08:40:05 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: d5055fab582741c725c1f9d1d9e57815baa9219b

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___l_i_s3_m_d_l.html#a540e6cd2a6538b9cec9d47985a7c678b',1,'Adafruit_LIS3MDL']]]
+  ['begin_5fi2c',['begin_I2C',['../class_adafruit___l_i_s3_m_d_l.html#aac9f3bde4b7bcaa61a5e46db7f46d3d0',1,'Adafruit_LIS3MDL']]],
+  ['begin_5fspi',['begin_SPI',['../class_adafruit___l_i_s3_m_d_l.html#a4ce162f4210290957a06abef419109b7',1,'Adafruit_LIS3MDL::begin_SPI(uint8_t cs_pin, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___l_i_s3_m_d_l.html#ae2b7e1510f6e00f4fc352d8502131975',1,'Adafruit_LIS3MDL::begin_SPI(int8_t cs_pin, int8_t sck_pin, int8_t miso_pin, int8_t mosi_pin)']]]
 ];
