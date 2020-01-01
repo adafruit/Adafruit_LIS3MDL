4d5b0dbf8912ce86f4747109822628aac14985d4
Doxygen CI <travis@travis-ci.org>
2020-01-01 01:13:38 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['selftest',['selfTest',['../class_adafruit___l_i_s3_m_d_l.html#ab1bcebba8e8c530eac77e4fa62a55551',1,'Adafruit_LIS3MDL']]],
   ['setdatarate',['setDataRate',['../class_adafruit___l_i_s3_m_d_l.html#a6c47185e12e334a8c6319b251e435a7e',1,'Adafruit_LIS3MDL']]],
   ['setintthreshold',['setIntThreshold',['../class_adafruit___l_i_s3_m_d_l.html#a36de10bf12d1af49abb8e41eed5cd84e',1,'Adafruit_LIS3MDL']]],
   ['setoperationmode',['setOperationMode',['../class_adafruit___l_i_s3_m_d_l.html#a528357269d29085002a141ec3b0e7c41',1,'Adafruit_LIS3MDL']]],
